a88e72eb6954fb35f5ca5c59d950c95d8cb936b5
uahma <uahma001@doc.gold.ac.uk>
2024-11-29 21:42:04 +0000

    Tweaking menu route

--- routes/items.js
+++ routes/items.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 // Handling route to display the menu
-router.get('/menu', function(req, res, next) {
+router.get('/', function(req, res, next) {
     let sqlquery = "SELECT * FROM menu"; // query database to get all the menu items
     // executing sql query
     db.query(sqlquery, (err, result) => {
