62d58d02da0b26a61963b6946ef6fbfc92677590
uahma <uahma001@doc.gold.ac.uk>
2024-11-29 21:59:50 +0000

    Adding test data

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,6 +3,10 @@
 USE umairs_restaurant;
 INSERT INTO menu (name, price, vegan) VALUES
     ('Chicken Fillet Burger', 3.99, FALSE),
-    ('French Fries', 1.99, FALSE), 
+    ('French Fries', 1.99, TRUE),
+    ('Cheeseburger', 2.99, FALSE),
+    ('Chicken Nuggets', 2.49, FALSE),
+    ('Cheese Pizza', 5.99, FALSE),
+    ('Vegetable Soup', 2.49, TRUE), 
     ('Fruit Salad', 2.99, TRUE);
 
