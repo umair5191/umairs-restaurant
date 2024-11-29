f9a5658cf437087083996ffff7efddf95d43b315
uahma <uahma001@doc.gold.ac.uk>
2024-11-29 19:16:30 +0000

    Tweaking index.js and connecting to database

--- index.js
+++ index.js
@@ -16,12 +16,41 @@ app.set('view engine', 'ejs');
 // Setting up the body parser
 app.use(express.urlencoded({ extended: true }));
 
-// Loading the route handlers
-const mainRoutes = require("./routes/main");
-app.use('/', mainRoutes);
-
 // Setting up public folder (for css and statis js)
 app.use(express.static('public'));
+
+// Creating a session
+app.use(session({
+    secret: 'secretkey',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}));
+
+// Setting up the database connection
+const db = mysql.createConnection ({
+    host: 'localhost',
+    user: 'umairs_restaurant_app',
+    password: 'restaurant123',
+    database: 'umairs_restaurant'
+});
+
+// Connecting to the database
+db.connect((err) => {
+    if (err) {
+        console.log('Error connecting to database', err);
+    }
+    else {
+        console.log('Connected to the database');
+    }
+});
+global.db = db;
+
+// Loading the route handlers for index and about pages
+const mainRoutes = require("./routes/main");
+app.use('/', mainRoutes);
 
 // Starting the server on port 8000
 app.listen(port, () => {
