44ebf26bbacdc3d306a1c3f64acf77e13ea85dfe
uahma <uahma001@doc.gold.ac.uk>
2024-11-29 20:10:55 +0000

    Adding data to the tables

---
+++ insert_test_data.sql
@@ -0,0 +1,8 @@
+# Inserting data into the tables
+
+USE umairs_restaurant;
+INSERT INTO menu (name, price, vegan) VALUES
+    ('Chicken Fillet Burger', 3.99, FALSE),
+    ('French Fries', 1.99, FALSE), 
+    ('Fruit Salad', 2.99, TRUE);
+
