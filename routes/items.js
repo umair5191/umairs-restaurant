8f0cd494711c7cdecf64aa7cad38db2933b0d45d
uahma <uahma001@doc.gold.ac.uk>
2024-11-30 00:07:53 +0000

    Handling route for search function

--- routes/items.js
+++ routes/items.js
@@ -38,4 +38,20 @@ router.get('/saver', function(req, res, next) {
      })
 })
 
+// Handling route to search for items
+router.get('/search',function(req, res, next){
+    res.render("search.ejs")
+})
+router.get('/search_result', function (req, res, next) {
+    // Search the database
+    let sqlquery = "SELECT * FROM menu WHERE name LIKE '%" + req.query.search_text + "%'"; // query database to get result
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err);
+        }
+        res.render("menu.ejs", {menuItems: result});
+     }) 
+})
+
 module.exports = router;
