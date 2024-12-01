22e979663313c2a8fe40c0150ad44e26b722ebd1
uahma <uahma001@doc.gold.ac.uk>
2024-12-01 04:26:50 +0000

    Checking if input is valid using validator

--- routes/items.js
+++ routes/items.js
@@ -1,6 +1,7 @@
 // Creating router for menu items
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require('express-validator'); // Importing express-validator for checking price is valid
 
 // Handling route to display the menu
 router.get('/', function(req, res, next) {
@@ -40,20 +41,51 @@ router.get('/saver', function(req, res, next) {
 
 // Handling route to search for items
 router.get('/search',function(req, res, next){
-    res.render("search.ejs")
+    res.render("search.ejs");
 })
 router.get('/search_result', function (req, res, next) {
     // Sanitizing the search to prevents harmful attacks
     sanitizedText = req.sanitize(req.query.search_text);
+
     // Search the database
     let sqlquery = "SELECT * FROM menu WHERE name LIKE '%" + sanitizedText + "%'"; // query database to get result
     // execute sql query
-    db.query(sqlquery, (err, result) => {
+    db.query(sqlquery, (err, result) => { // selecting matching items
         if (err) {
             next(err);
         }
         res.render("search_results.ejs", {menuItems: result}); // displaying the search results
      }) 
 })
+
+// Handling route to add a new item to menu
+router.get('/add', function (req, res, next) {
+    res.render('add.ejs');
+})
+router.post('/itemadded', [check('price').isDecimal(), check('name').notEmpty()], function (req, res, next) { // checks if price is a valid decimal and name isn't empty
+    // Sanitiizing the input to prevent harmful attacks
+    let name = req.sanitize(req.body.name);
+    let price = req.sanitize(req.body.price);
+    let vegan = req.body.vegan === 'true'; // converting to boolean
+
+    // Checking if inputs were valid using express-validator
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // Makes user input data again if invalid
+        res.redirect('/menu/add');
+    }
+
+    // saving data in database
+    let sqlquery = "INSERT INTO menu (name, price, vegan) VALUES (?,?,?)"
+    // executing sql query
+    let newrecord = [name, price, vegan];
+    db.query(sqlquery, newrecord, (err, result) => { // adding item to menu
+        if (err) {
+            next(err);
+        }
+        else
+            res.send(' This item has been added to our menu: Name: '+ name + ', Price: '+ price + ', Vegan: '+ vegan);
+    })
+}) 
 
 module.exports = router;
