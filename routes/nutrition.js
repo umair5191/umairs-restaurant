0f1d4e760074628dbab80c49bcbd5edf7e79de14
uahma <uahma001@doc.gold.ac.uk>
2024-12-11 03:02:36 +0000

    Updating paths to the views

--- routes/nutrition.js
+++ routes/nutrition.js
@@ -1,11 +1,11 @@
 // This file defines the routes for the nutritional information API (API Ninjas)
 const express = require('express');
 const router = express.Router();
-const axios = require('axios'); // Use axios for making HTTP requests as request is outdated
+const axios = require('axios'); // Using axios for making HTTP requests as request is outdated
 
 // Handling the nutrition page
 router.get('/', (req, res) => {
-    res.render('nutrition'); // Displays page for user to search for nutritional information
+    res.render('nutrition/nutrition.ejs'); // Displays page for user to search for nutritional information
 });
 router.post('/search', (req, res, next) => {
     let item = req.body.query; // Assigning user input to item
@@ -14,9 +14,9 @@ router.post('/search', (req, res, next) => {
     axios.get(url, { headers: { 'X-Api-Key': '4r3sGBRY4X0BM79J+4ZsMA==yGBBfoKUnS0N6x2v' } })
         .then((response) => {
             if (response.data.length > 0) { // If there's a match, the nutritional information is displayed
-                res.render('./nutrition_result', { item, nutrition: response.data[0] });
+                res.render('nutrition/nutrition_result.ejs', { item, nutrition: response.data[0] });
             } else {
-                res.render('nutrition_result', { item, nutrition: null }); // Informing user that no match was found
+                res.render('nutrition/nutrition_result.ejs', { item, nutrition: null }); // Informing user that no match was found
             }
         })
         .catch((err) => {
--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ router.get('/register', function (req, res, next) {
 router.post('/registered', [check('email').isEmail(), check('password').isLength({ min: 10 }), check('first').notEmpty(), check('last').notEmpty(), check('username').notEmpty()], function (req, res, next) {
     const errors = validationResult(req);  
     if (!errors.isEmpty()) {
-        res.render('register/register_error');  // If there are errors, user is informed and prompted to try again
+        res.render('register/register_error.ejs');  // If there are errors, user is informed and prompted to try again
     }
     else {
         const plainPassword = req.sanitize(req.body.password); // Sanitizing the password to prevent harmful attacks
@@ -31,7 +31,7 @@ router.post('/registered', [check('email').isEmail(), check('password').isLength
                 if (err) {
                     next(err);
                 }
-                res.render('register/user_registered', {first}); // If successful, user is informed that they have been registered
+                res.render('register/user_registered.ejs', {first}); // If successful, user is informed that they have been registered
             })                                                                           
         })
     }
@@ -40,7 +40,7 @@ router.post('/registered', [check('email').isEmail(), check('password').isLength
 // Handling route to login
 router.get('/login', function (req, res, next) {
     if (req.session.userId) { // Checking if user is already logged in
-        res.render('login/login_again', {username: req.session.userId}); // If user is already logged in, they are informed and prompted to logout
+        res.render('login/login_again.ejs', {username: req.session.userId}); // If user is already logged in, they are informed and prompted to logout
     }
     else {
         res.render('login/login.ejs'); // If user is not logged in, they are prompted to login
@@ -53,7 +53,7 @@ router.post('/loggedin', function (req, res, next) {
             next(err);
         }
         if (result.length == 0) {
-            return res.render('login/login_error'); // If username is not found, user is informed and prompted to try again
+            return res.render('login/login_error.ejs'); // If username is not found, user is informed and prompted to try again
         }
         let hashedPassword = result[0].hashedPassword;
         // Comparing the password supplied with the password in the database
@@ -61,10 +61,10 @@ router.post('/loggedin', function (req, res, next) {
             if (result == true) {
                 // Saving user session here, when login is successful
                 req.session.userId = req.sanitize(req.body.username);
-                res.render('login/loggedin', {username: req.sanitize(req.body.username)}); // If password is correct, user is informed that they are logged in
+                res.render('login/loggedin.ejs', {username: req.sanitize(req.body.username)}); // If password is correct, user is informed that they are logged in
             }
             else {
-                res.render('login/login_error'); // If password is incorrect, user is informed and prompted to try again
+                res.render('login/login_error.ejs'); // If password is incorrect, user is informed and prompted to try again
             }
         })
     })
@@ -73,11 +73,11 @@ router.post('/loggedin', function (req, res, next) {
 // Handling route to logout
 router.get('/logout', function (req, res, next) {
     if (!req.session.userId) { // Checking if user is logged in
-        res.render('logout/logout_error'); // If user is not logged in, they are informed that they cannot log out and prompted to login
+        res.render('logout/logout_error.ejs'); // If user is not logged in, they are informed that they cannot log out and prompted to login
     }
     else {
         req.session.destroy(); // If user is logged in, their session is destroyed
-        res.render('logout/loggedout'); // User is informed that they have been logged out
+        res.render('logout/loggedout.ejs'); // User is informed that they have been logged out
     }
 })
 
