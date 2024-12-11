1a6d7be0e13f69a6e0e128ada92f5c60d4ed1708
uahma <uahma001@doc.gold.ac.uk>
2024-12-11 22:14:37 +0000

    Correcting syntax

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -8,15 +8,15 @@ INSERT INTO menu (name, price, vegan) VALUES
     ('Chicken Nuggets', 5.49, FALSE),
     ('Cheese Pizza', 5.99, FALSE),
     ('Vegetable Soup', 6.49, TRUE), 
-    ('Fruit Salad', 2.99, TRUE);
-    ('Apple Pie', 1.99, TRUE);
-    ('Chocolate Cake', 12.99, TRUE);
-    ('Apple Juice', 1.49, TRUE);
-    ('Orange Juice', 1.49, TRUE);
-    ('Coca Cola', 1.49, TRUE);
-    ('Pepsi', 1.49, TRUE);
-    ('Water', 0.99, TRUE);
-    ('Strawberries', 2.50, TRUE);
+    ('Fruit Salad', 2.99, TRUE),
+    ('Apple Pie', 1.99, TRUE),
+    ('Chocolate Cake', 12.99, TRUE),
+    ('Apple Juice', 1.49, TRUE),
+    ('Orange Juice', 1.49, TRUE),
+    ('Coca Cola', 1.49, TRUE),
+    ('Pepsi', 1.49, TRUE),
+    ('Water', 0.99, TRUE),
+    ('Strawberries', 2.50, TRUE),
     ('Fish Fingers', 8.99, FALSE);
     
 
